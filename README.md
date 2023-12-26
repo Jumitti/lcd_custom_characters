4c2c4e575f4503ce45a81250670e75f0de07f343
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-12-26 20:41:07 +0100

    Update

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Custom Characters LCD
 Streamlit app to generate custom characters for LCD 16x2
 
-[Streamlit App](https://custom-characters-for-lcd16x2.streamlit.app/)
+This generator is for LCD16x2 presented on the [Github LCD](https://github.com/the-raspberry-pi-guy/lcd?tab=readme-ov-file#custom-characters)
+
+Original implementation of custom characters by Juvus: [Custom Characters](https://github.com/juvus/lcd)
+
+Streamlit app by Julien Minniti: [Streamlit Custom Characters LCD Generator](https://custom-characters-for-lcd16x2.streamlit.app/)
+
 
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-with open('main.html', 'r', encoding='utf-8') as file:
-    grid_content = file.read()
-
 st.title("Custom Characters for LCD 16x2")
 
-components.html(grid_content,
-    height=600)
+st.markdown('This generator is for LCD16x2 presented on the [Github LCD](https://github.com/the-raspberry-pi-guy/lcd?tab=readme-ov-file#custom-characters)', unsafe_allow_html=True)
+st.markdown('Original implementation of custom characters by Juvus: [Custom Characters](https://github.com/juvus/lcd)', unsafe_allow_html=True)
+st.markdown('Source code of this Streamlit app by Julien Minniti: [Streamlit Custom Characters LCD Generator](https://github.com/Jumitti/lcd_custom_characters)', unsafe_allow_html=True)
+
+with open('main.html', 'r', encoding='utf-8') as file:
+    grid_content = file.read()
+    file.close()
+
+components.html(grid_content, height=600)
